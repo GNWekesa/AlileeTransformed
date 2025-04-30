bf93b7e1e80f09c5e6e28c26b80d2119b798de80
GNWekesa <gabriellanekwek@gmail.com>
2025-05-01 00:06:47 +0300

    Day 1- 30th April 2025 I initialized node.js in my root folder using the command npm init –y I then installed boostrap using the command npm install bootstrap@5.3.5 I now make my project structure to be AlileeTransformed/ │ ├── node_modules/ ├── public/ │   ├── css/ │   ├── js/ ├── index.html ├── app.js ├── package.json In app.js, I wrote the code for the server

---
+++ app.js
@@ -0,0 +1,29 @@
+// Import the Express framework
+const express = require('express');
+
+// Import the path module (to handle file paths)
+const path = require('path');
+
+// Initialize the Express application
+const app = express();
+
+// Middleware: Serve static files (CSS, JS, Images) from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+//added on day 2, to Serve static files from 'node_modules'
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+// Middleware: Serve Bootstrap static files from node_modules
+// Allows me to use Bootstrap via "/bootstrap" URL path
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+
+// Route: Handle GET request for the homepage "/"
+// Sends the 'index.html' file as the response
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server and listen on port 3000
+// Console log a message once the server is running
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
